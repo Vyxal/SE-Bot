6a5e6ffe45662736ba1870e8bea49e5edd49ec5c
hyper-neutrino <alex.yj.liao@gmail.com>
2022-04-26 17:17:44 -0400

    E

--- src/server.js
+++ src/server.js
@@ -13,11 +13,11 @@ app.use(bodyparser.json());
 app.use(bodyparser.raw());
 
 app.use((req, res, next) => {
-    console.log(req.body);
-    return;
-
     const hmac = crypto.createHmac("sha256", config.github_secret);
     const data = hmac.update(req.body);
+    const hex = data.digest("hex");
+
+    console.log(hex);
 
     console.log(req.headers);
     next();
